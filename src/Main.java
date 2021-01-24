173cc0cd8148b9fd195ccbf48d16abb868a6bd80
Denis T <dengertd@gmail.com>
2021-01-24 23:03:53 +0300

    added

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 12_000;
+        int miles = 1;
+        int count = 20;
+
+        System.out.println(price / count * miles);
+    }
+}
